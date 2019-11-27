fab935c0d653a1f73e15bff827916548846ba154
Owner <Owner@CPU0198.alliance.com.ph>
2019-11-27 16:31:52 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-samplebydaveg"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2gxnit8wv3.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_at7zA929R",
+    APP_CLIENT_ID: "7ols9gd83qdr0qcsuesn3iprma",
+    IDENTITY_POOL_ID: "us-east-1:9dc6276d-3f0d-4320-8a3a-e1e597bb0131"
+  }
+};
